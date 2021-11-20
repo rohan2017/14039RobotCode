1560e606578f668f13319663180218f6862de734
rohan2017 <rohanbandaru1@gmail.com>
2021-11-20 14:14:06 -0500

    Update SCurve.java

--- 14039RobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/SCurve.java
+++ 14039RobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/SCurve.java
@@ -9,24 +9,42 @@ public class SCurve extends Controller {
     private double maxVel = 1;
 
     private double t, d, c;
+    private double crossover;
+    private double accelDistOffset, decelDistOffset;
 
     public SCurve(double distance) {
         this.distance = distance;
         t = Math.log(maxVel/minVel - 1) / maxVel;
         d = t/decelerationSlope + distance;
         c = t/accelerationSlope;
+        crossover = (accelerationSlope*c - decelerationSlope*d) / (accelerationSlope-decelerationSlope);
+        accelDistOffset = (Math.log(Math.pow(Math.E, accelerationSlope*maxVel*c) + 1)/accelerationSlope) - minVel;
+        decelDistOffset = 0;
+        decelDistOffset = evaluateDistance(crossover+0.0001) - evaluateDistance(crossover);
         correction = 0;
     }
 
     public void update(double setPoint, double currentDistance) {
         double input = distance - currentDistance;
-        double accelcorrect = maxVel / (1 + Math.pow(Math.E, (accelerationSlope * maxVel * (c - input))));
-        double deccelcorrect = maxVel / (1 + Math.pow(Math.E, (decelerationSlope * maxVel * (d - input))));
+        correction = evaluateVelocity(input);
+    }
 
-        if(accelcorrect <= deccelcorrect) {
-            correction = accelcorrect;
-        }else {
-            correction = deccelcorrect;
+    public double evaluateVelocity(double input) {
+        if(input <= crossover) {
+            return maxVel / (1 + Math.pow(Math.E, (accelerationSlope * maxVel * (c - input))));
+        }else if(input > crossover) {
+            return maxVel / (1 + Math.pow(Math.E, (decelerationSlope * maxVel * (d - input))));
         }
+    }
+
+    public double evaluateDistance(double input) {
+        if(input <= crossover) {
+            double tmp = Math.log(Math.pow(Math.E, accelerationSlope*maxVel*(c-input)) + 1);
+            return (tmp/accelerationSlope) + (maxVel * input) - accelDistOffset;
+        }else {
+            double tmp = Math.log(Math.pow(Math.E, decelerationSlope*maxVel*(d-input)) + 1);
+            return (tmp/decelerationSlope) + (maxVel * input) - decelDistOffset;
+        }
+
     }
 }
