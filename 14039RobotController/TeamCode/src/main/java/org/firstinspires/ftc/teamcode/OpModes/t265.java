6964bd5e0e5562cc789688024bf01a8d0090ef20
rohan2017 <rohanbandaru1@gmail.com>
2021-12-04 19:14:08 -0500

    t265

---
+++ 14039RobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/t265.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Transform2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.spartronics4915.lib.T265Camera;
+
+@TeleOp(name="Test T265", group="Tests")
+public class t265 extends OpMode
+{
+    private static T265Camera slamra = null;
+    Pose2d startingPose = new Pose2d(1, 1, new Rotation2d());
+
+
+    @Override
+    public void init() {
+        if (slamra == null) {
+            slamra = new T265Camera(new Transform2d(), 0.1, hardwareMap.appContext);
+            slamra.setPose(startingPose);
+        }
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        slamra.start();
+    }
+
+    @Override
+    public void loop() {
+
+        T265Camera.CameraUpdate up = slamra.getLastReceivedCameraUpdate();
+
+        if (up == null) {
+            return;
+        }
+
+        telemetry.addData("data", up);
+        telemetry.update();
+
+    }
+
+    @Override
+    public void stop() {
+        slamra.stop();
+    }
+
+}
