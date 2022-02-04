02c36f9b61f0c0ee33e7f77b341b9df3a5c91e27
Rohan B <rohanbandaru1@gmail.com>
2022-02-04 14:05:26 -0500

    matching format to master

--- OLD14039RobotControllerOLD/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/Controller.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.Controllers;
-
-public abstract class Controller {
-
-    public double error, correction;
-
-    public void update(double setPoint, double current) {}
-
-}
